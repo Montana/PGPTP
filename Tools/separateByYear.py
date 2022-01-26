88db0ec9698306d3d372643ef58192966267d8cc
Montana Mendy <montana@linux.com>
2022-01-26 13:48:01 -0800

    Updated formatting.

--- Tools/separateByYear.py
+++ Tools/separateByYear.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 '''
 Created on Oct 13, 2014
-
 @author: waldo
 '''
 
@@ -9,18 +8,20 @@ import sys
 import os
 import csv
 
+
 def makeOutFile(outdict, term, fname, header):
     outfile = csv.writer(open(term + '/' + fname, 'w'))
     outdict[term] = outfile
     outfile.writerow(header)
 
+
 if __name__ == '__main__':
     if len(sys.argv) < 1:
         print 'Usage: separateByYear csvFile'
         sys.exit(1)
-        
+
     fname = sys.argv[1]
-    
+
     fin = csv.reader(open(fname, 'rU'))
     header = fin.next()
     outdict = {}
